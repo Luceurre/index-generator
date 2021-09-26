6de1bdce0a90ea76425e06696f5a483eb1429adf
Pierre Glandon <pglandon78@gmail.com>
2021-09-26 19:34:56 +0200

    fix: await async test

--- src/utils/tests/getFilePathsMatching.test.ts
+++ src/utils/tests/getFilePathsMatching.test.ts
@@ -1,22 +1,31 @@
 import { getFilePathsMatching } from '../getFilePathsMatching';
+import * as path from 'path';
 
-const FIXTURES_BASE_DIR = `${__dirname}/fixtures/getFilePathMatchingFixtures/`;
+const FIXTURES_BASE_DIR = `${__dirname}/fixtures/getFilePathsMatchingFixtures/`;
 
 describe('getFilePathsMatching', function () {
-    it('should rejects if working directory is not found', function () {
-        const cwd = '/not_an_existing_directory';
-        expect(getFilePathsMatching(/a regex/, cwd)).rejects.toStrictEqual('Argument workingDir should be an existing directory.');
-    });
+  it('should rejects if working directory is not found', async function () {
+    const cwd = '/not_an_existing_directory';
+    await expect(getFilePathsMatching(/a regex/, cwd)).rejects.toStrictEqual(
+      'Argument workingDir should be an existing directory.',
+    );
+  });
 
-    it('should rejects if working directory is a file', function () {
-        expect(getFilePathsMatching(/a regex/, FIXTURES_BASE_DIR + 'not_a_directory.txt')).rejects.toStrictEqual('Argument workingDir should be an existing directory.');
-    });
+  it('should rejects if working directory is a file', async function () {
+    await expect(getFilePathsMatching(/a regex/, FIXTURES_BASE_DIR + 'not_a_directory.txt')).rejects.toStrictEqual(
+      'Argument workingDir should be an existing directory.',
+    );
+  });
 
-    it('should should not look in subdirectory', function () {
-        expect(getFilePathsMatching(/.*/, FIXTURES_BASE_DIR)).resolves.toStrictEqual(['not_a_directory.txt']);
-    });
+  it('should should not look in subdirectory', async function () {
+    await expect(getFilePathsMatching(/.*/, FIXTURES_BASE_DIR)).resolves.toStrictEqual(
+      ['Component.tsx', 'index.ts', 'not_a_directory.txt'].map((filePath) => path.join(FIXTURES_BASE_DIR, filePath)),
+    );
+  });
 
-    it('should only return files matching regex', function () {
-        expect(getFilePathsMatching(/\.tsx?/, FIXTURES_BASE_DIR)).resolves.toStrictEqual(['index.ts', 'Component.tsx']);
-    });
+  it('should only return files matching regex', async function () {
+    await expect(getFilePathsMatching(/\.tsx?/, FIXTURES_BASE_DIR)).resolves.toStrictEqual(
+      ['Component.tsx', 'index.ts'].map((filePath) => path.join(FIXTURES_BASE_DIR, filePath)),
+    );
+  });
 });
